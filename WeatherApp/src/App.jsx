b222407f5f2b03eae6953257d38fbcf2c851517b
Mahdi Khodaei <mahdi.khodaei79@gmail.com>
2024-08-05 17:59:27 +0330

    Print city name in thair component

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function AddDataSpace({location}){
   const [city , setCity] = useState("");
-  const Submit = () => location(city);
+  const Submit = () => {
+    location(city);
+    setCity("");
+  }
 
   return(
     <div className="AddDataSpace">
       <input type="text"
        id="AddDataText"
-        placeholder='Write The city Here 🏙'
+        placeholder='Write The city Here  🏙'
         value={city}
-        onChange={(e) => setCity(e.target.value)}
+        onChange={(e) => setCity(e.target.value.toLowerCase())}
         />
         <button
         id='SubmitDataText'
         onClick={Submit}
-        >Submit</button>
+        >Get Weather</button>
     </div>
 
   );
-}
+};
+
+function ShowDataSpace({city}){
+
+  return(
+    <div className="ShowDataSpace">
+
+      <p className="CityName">
+        {city.toUpperCase()}
+      </p>
+    </div>
+  );
+};
+
+
+
+
 
 
 export default function App(){
@@ -28,22 +47,16 @@ export default function App(){
 
   function location(newcity){
     setCity(newcity);
-  }
-
-  async function GetWeather(){
-
-    try{
-
-    }
-    catch
 
   }
+  useEffect( () => {console.log(city);}, [city])
 
 
 
   return(
     <>
     <AddDataSpace location={location} />
+    < ShowDataSpace city={city} />
     </>
      );
 };
